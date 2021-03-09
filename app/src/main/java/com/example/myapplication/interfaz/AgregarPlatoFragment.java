b60f33aabeaaffb4979b4e0ec6b629c60bdd81ec
sebasrm98 <79673528+sebasrm98@users.noreply.github.com>
2021-03-09 13:59:20 -0500

    version 1.0: modulo interfaz

--- app/src/main/java/com/example/myapplication/interfaz/AgregarPlatoFragment.java
+++ app/src/main/java/com/example/myapplication/interfaz/AgregarPlatoFragment.java
@@ -29,6 +29,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
