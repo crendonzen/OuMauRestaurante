e8c290f018eaa5c5e55221504233aa35032b8cdc
sebasrm98 <79673528+sebasrm98@users.noreply.github.com>
2021-04-07 21:59:51 -0500

    version 1.0: version servidor terminado

--- app/src/main/java/com/example/myapplication/adaptador/Servidor.java
+++ app/src/main/java/com/example/myapplication/adaptador/Servidor.java
@@ -2,7 +2,7 @@ package com.example.myapplication.adaptador;
 
 public class Servidor {
 
-    public static final String HOST=" http://192.168.1.5";
+    public static final String HOST="https://phpclusters-26810-0.cloudclusters.net";
 
 
 }
